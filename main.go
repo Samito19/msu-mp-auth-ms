076bf432813f6398a07597f47f9c600f90cf6573
Sami Amsaf <samiamsaf1902@gmail.com>
2023-01-07 12:13:18 +0100

    Implemented HTTP Server along with /athenticate path to retreive inputed credentials

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+
+	. "github.com/Samito19/msu-mp-auth-ms/user"
+)
+
+func main() {
+	functionsHandler()
+	http.ListenAndServe(":3001", nil)
+}
+
+func functionsHandler() {
+	http.HandleFunc("/authenticate", handleAuthentication)
+}
+
+func handleAuthentication(w http.ResponseWriter, req *http.Request) {
+	var user User
+	credentials, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		panic(err)
+	}
+	json.Unmarshal(credentials, &user)
+	fmt.Println(user.EmailAddress)
+}

528c8f4a4fc180eb6db1f06cd1a8f00ae74afc59
Sami Amsaf <samiamsaf1902@gmail.com>
2023-01-09 12:22:51 +0100

    Added encryption functionality

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
 	"net/http"
 
@@ -25,5 +24,5 @@ func handleAuthentication(w http.ResponseWriter, req *http.Request) {
 		panic(err)
 	}
 	json.Unmarshal(credentials, &user)
-	fmt.Println(user.EmailAddress)
+
 }
